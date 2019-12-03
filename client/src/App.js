38e5d99d36f9a838e45bfe2beb3d44debdacdd73
unknown <michaelperezabreu@gmail.com>
2019-12-02 18:19:33 -0800

    Added userActions and authActions to redux store

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import AppNavBar from "./components/AppNavBar";
 import Bookshelf from "./components/Bookshelf";
 
 import { Provider } from "react-redux";
 
 import store from "./store";
+import { loadUser } from "./actions/authActions";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
-function App() {
-  return (
-    <Provider store={store}>
-      <div className="App">
-        <AppNavBar />
-        <Bookshelf key="Bookshelf" />
-      </div>
-    </Provider>
-  );
+class App extends Component {
+  componentDidMount() {
+    store.dispatch(loadUser());
+  }
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <AppNavBar />
+          <Bookshelf key="Bookshelf" />
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/actions/authActions.js
@@ -0,0 +1,117 @@
+import axios from "axios";
+import { returnErrors } from "./errorActions";
+
+import {
+  USER_LOADED,
+  USER_LOADING,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT_SUCCESS,
+  REGISTER_SUCCESS,
+  REGISTER_FAIL
+} from "./types";
+
+//Check for token
+export const loadUser = () => (dispatch, getState) => {
+  //loading user
+  dispatch({ type: USER_LOADING });
+
+  //Authenticate user
+  axios
+    .get("/api/auth/user", tokenConfig(getState))
+    .then(res =>
+      dispatch({
+        type: USER_LOADED,
+        payload: res.data
+      })
+    )
+    .catch(err => {
+      dispatch(returnErrors(err.response.data, err.response.status));
+      dispatch({
+        type: AUTH_ERROR
+      });
+    });
+};
+
+//REgister user
+export const register = ({ name, email, password }) => dispatch => {
+  //header
+  const config = {
+    headers: { "Content-Type": "application/json" }
+  };
+
+  //Body
+  const body = JSON.stringify({ name, email, password });
+
+  axios
+    .post("/api/users", body, config)
+    .then(res =>
+      dispatch({
+        type: REGISTER_SUCCESS,
+        payload: res.data
+      })
+    )
+    .catch(err => {
+      dispatch(
+        returnErrors(err.response.data, err.response.status, "REGISTER_FAIL")
+      );
+      dispatch({
+        type: REGISTER_FAIL
+      });
+    });
+};
+
+//LogIn User
+export const login = ({ email, password }) => dispatch => {
+  //header
+  const config = {
+    headers: { "Content-Type": "application/json" }
+  };
+
+  //Body
+  const body = JSON.stringify({ email, password });
+
+  axios
+    .post("/api/auth", body, config)
+    .then(res =>
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data
+      })
+    )
+    .catch(err => {
+      dispatch(
+        returnErrors(err.response.data, err.response.status, "LOGIN_FAIL")
+      );
+      dispatch({
+        type: LOGIN_FAIL
+      });
+    });
+};
+
+//Logout User
+export const logout = () => {
+  return {
+    type: LOGOUT_SUCCESS
+  };
+};
+
+export const tokenConfig = getState => {
+  //Get token from localstorage
+  const token = getState().auth.token;
+
+  //Header
+  const config = {
+    headers: {
+      "Content-type": "application/json"
+    }
+  };
+
+  //if there's a token add it
+  if (token) {
+    config.headers["x-auth-token"] = token;
+  }
+
+  return config;
+};
