9835897d145b0ac0ed049f0bec239fc2e326cca7
unknown <michaelperezabreu@gmail.com>
2019-12-05 19:40:35 -0800

    Added texttoSpeech component for audio book

--- client/src/components/ProfileModal.js
+++ client/src/components/ProfileModal.js
@@ -1,7 +1,20 @@
 import React, { Component } from "react";
 import pcimage from "../image/open_book-512.png";
-import { Button, Modal, ModalHeader, ModalBody, Media } from "reactstrap";
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Media,
+  Container
+} from "reactstrap";
 import EditorModal from "./EditorModal";
+import ReactHtmlParser, {
+  processNodes,
+  convertNodeToElement,
+  htmlparser2
+} from "react-html-parser";
+import Speech from "react-speech";
 
 class ProfileModal extends Component {
   state = {
@@ -35,47 +48,51 @@ class ProfileModal extends Component {
 
     return (
       <div>
-        <Button
-          color="primary"
-          style={{ marginBottom: "2rem" }}
-          onClick={this.toggle}
-          block
-        >
-          Book Profile
-        </Button>
+        <Container>
+          <Button
+            color="primary"
+            style={{ marginBottom: "2rem" }}
+            onClick={this.toggle}
+            block
+          >
+            Book Profile
+          </Button>
 
-        <Modal
-          size="lg"
-          centered
-          isOpen={this.state.modal}
-          toggle={this.toggle}
-        >
-          <ModalHeader toggle={this.toggle}>
-            <Media>
-              <Media left href="#">
-                <Media object src={pcimage} alt="Generic placeholder image" />
+          <Modal
+            size="lg"
+            centered
+            isOpen={this.state.modal}
+            toggle={this.toggle}
+          >
+            <ModalHeader toggle={this.toggle}>
+              <Media>
+                <Media left href="#">
+                  <Media object src={pcimage} alt="Generic placeholder image" />
+                </Media>
+                <Media body>
+                  <Media heading>{book.title}</Media>
+                </Media>
               </Media>
-              <Media body>
-                <Media heading>{book.title}</Media>
-              </Media>
-            </Media>
-          </ModalHeader>
+            </ModalHeader>
 
-          <ModalBody>
-            Escrito por: {book.author} en fecha: {book.pubdate}
-            Una breve descripcion estaria aqui: Subido enL {book.upldate}
-          </ModalBody>
-          <EditorModal
-            readmode={true}
-            htmlcontent={htmlcontent}
-            booktitle={book.title}
-            bookauthor={book.author}
-            bookpublisher={book.publisher}
-            buttonColor={"primary"}
-            buttonType={"Leer Libro"}
-          />
-          <Button block>Escuchar Libro </Button>
-        </Modal>
+            <ModalBody>
+              Escrito por: {book.author} en fecha: {book.pubdate}
+              Una breve descripcion estaria aqui: Subido enL {book.upldate}
+            </ModalBody>
+            <EditorModal
+              readmode={true}
+              htmlcontent={htmlcontent}
+              booktitle={book.title}
+              bookauthor={book.author}
+              bookpublisher={book.publisher}
+              buttonColor={"primary"}
+              buttonType={"Leer Libro"}
+            />
+            <Container>
+              <Speech text={htmlcontent.replace(/<[^>]*>?/gm, "")} />
+            </Container>
+          </Modal>
+        </Container>
       </div>
     );
   }
